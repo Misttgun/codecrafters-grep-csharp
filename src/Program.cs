68bf2d624c31ca8fca6d4675425dd17705b197a4
Maurel Sagbo <maurel.sagbo@gmail.com>
2025-09-15 14:54:09 +0200

    Match digits

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,6 @@
 using System;
 using System.IO;
 
-static bool MatchPattern(string inputLine, string pattern)
-{
-    if (pattern.Length == 1)
-    {
-        return inputLine.Contains(pattern);
-    }
-    else
-    {
-        throw new ArgumentException($"Unhandled pattern: {pattern}");
-    }
-}
-
 if (args[0] != "-E")
 {
     Console.WriteLine("Expected first argument to be '-E'");
@@ -23,10 +11,19 @@ string pattern = args[1];
 string inputLine = Console.In.ReadToEnd();
 
 if (MatchPattern(inputLine, pattern))
-{
     Environment.Exit(0);
-}
 else
-{
     Environment.Exit(1);
+
+return;
+
+static bool MatchPattern(string inputLine, string pattern)
+{
+    if (pattern == "\\d")
+        return inputLine.Any(char.IsDigit);
+    
+    if (pattern.Length == 1)
+        return inputLine.Contains(pattern);
+
+    throw new ArgumentException($"Unhandled pattern: {pattern}");
 }
