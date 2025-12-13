2d11fe4377688c7d42394d53fe28c6514f3ecc13
Maurel Sagbo <maurel.sagbo@gmail.com>
2025-12-13 16:34:42 +0100

    Print single match

---
+++ src/Parser.cs
@@ -0,0 +1,31 @@
+﻿namespace codecrafters_grep;
+
+public static class Parser
+{
+    public readonly record struct Options(string Pattern, bool PrintMatchesOnly);
+    
+    public static bool TryParseArgs(string[] args, out Options options)
+    {
+        options = default;
+        
+        // Required by the challenge (at least for now).
+        if (args.Contains("-E") == false)
+        {
+            Console.WriteLine("Expected arguments to contain '-E'");
+            return false;
+        }
+
+        var printMatchesOnly = args.Contains("-o");
+
+        // First non-flag token is the pattern.
+        var pattern = args.FirstOrDefault(a => a.Length > 0 && a[0] != '-');
+        if (string.IsNullOrEmpty(pattern))
+        {
+            Console.WriteLine("Expected a pattern argument");
+            return false;
+        }
+
+        options = new Options(pattern, printMatchesOnly);
+        return true;
+    }
+}
